b85ec14b05a9bf7b1cc483d8a30cc19725da73ee
norbusonam <nsonam1822@gmail.com>
2023-12-08 08:21:04 -0500

    expo router setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home</Text>
+      <Link href="/account">Account</Link>
     </View>
   );
 }
