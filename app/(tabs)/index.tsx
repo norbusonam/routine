c5cd4682a02d0b085be1c0807b3007643b1d63a1
norbusonam <nsonam1822@gmail.com>
2024-01-03 17:29:19 -0500

    basic tab navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Planner() {
+  return <Text>Planner</Text>;
+}
---
+++ app/(tabs)/projects.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Projects() {
+  return <Text>Projects</Text>;
+}
