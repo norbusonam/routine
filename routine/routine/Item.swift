e3049788996b96c817311a341fd0232fa68de619
norbusonam <nsonam1822@gmail.com>
2024-01-03 17:58:16 -0500

    initialize swift ui project

---
+++ routine/routine/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  routine
+//
+//  Created by Norbu Sonam on 1/3/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
