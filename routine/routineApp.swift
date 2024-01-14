b6be3f348b49a1a613e3ea79d3d837e29643f211
norbusonam <nsonam1822@gmail.com>
2024-01-14 00:46:04 -0800

    add routine to model container

--- routine/routineApp.swift
+++ routine/routineApp.swift
@@ -11,7 +11,7 @@ import SwiftData
 @main
 struct routineApp: App {
     var sharedModelContainer: ModelContainer = {
-        let schema = Schema([])
+        let schema = Schema([Routine.self])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
         do {
