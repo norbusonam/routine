6226eea6329e8574dcd6dfb705cd03cf86898b39
norbusonam <nsonam1822@gmail.com>
2024-01-14 00:45:08 -0800

    routine model

---
+++ routine/Models/Routine.swift
@@ -0,0 +1,33 @@
+//
+//  Routine.swift
+//  routine
+//
+//  Created by Norbu Sonam on 1/14/24.
+//
+
+import SwiftData
+
+enum RoutineType {
+    case positive, negative
+}
+
+enum Interval {
+    case day, week, month, year
+}
+
+enum DaysOfTheWeek {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+}
+
+@Model
+class Routine {
+    var name: String
+    var emoji: Character
+    var type: RoutineType
+    var goal: Int
+    var unit: String
+    var interval: Interval
+    var days: [DaysOfTheWeek]
+    
+    init() { }
+}
