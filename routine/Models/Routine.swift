c15641ad7a9ec5f8a5073ed19ef29a8ccb749dd1
norbusonam <nsonam1822@gmail.com>
2024-01-19 19:27:31 -0800

    fix swift data

--- routine/Models/Routine.swift
+++ routine/Models/Routine.swift
@@ -7,27 +7,35 @@
 
 import SwiftData
 
-enum RoutineType {
+enum RoutineType: String {
     case positive, negative
 }
 
-enum Interval {
+enum Interval: String {
     case day, week, month, year
 }
 
-enum DaysOfTheWeek {
+enum DaysOfTheWeek: String {
     case monday, tuesday, wednesday, thursday, friday, saturday, sunday
 }
 
-//@Model
-//class Routine {
-//    var name: String
-//    var emoji: Character
-//    var type: RoutineType
-//    var goal: Int
-//    var unit: String
-//    var interval: Interval
-//    var days: [DaysOfTheWeek]
-//    
-//    init() { }
-//}
+@Model
+class Routine {
+    var name: String
+    var emoji: String
+    var type: String
+    var goal: Int
+    var unit: String
+    var interval: String
+    var days: [String]
+    
+    init(name: String, emoji: String, type: RoutineType, goal: Int, unit: String, interval: Interval, days: [DaysOfTheWeek]) {
+        self.name = name
+        self.emoji = emoji
+        self.type = type.rawValue
+        self.goal = goal
+        self.unit = unit
+        self.interval = interval.rawValue
+        self.days = days.map(\.rawValue)
+    }
+}
