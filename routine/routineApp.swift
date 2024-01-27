a8b52652c542c8a9810ecf4548bcf24e38e00704
norbusonam <nsonam1822@gmail.com>
2024-01-26 22:18:12 -0800

    show view based on auth

--- routine/routineApp.swift
+++ routine/routineApp.swift
@@ -13,17 +13,23 @@ struct routineApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([Routine.self])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
-
+    
+    // TODO: auth check logic
+    let authenticated = true
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if (authenticated) {
+                AuthenticatedView()
+            } else {
+                AuthView()
+            }
         }
         .modelContainer(sharedModelContainer)
     }
