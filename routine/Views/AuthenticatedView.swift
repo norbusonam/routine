86e005446acc81e28f214672d5ec956a24cda845
norbusonam <nsonam1822@gmail.com>
2024-01-28 21:49:54 -0800

    navigation (buggy)

--- routine/Views/AuthenticatedView.swift
+++ routine/Views/AuthenticatedView.swift
@@ -8,29 +8,13 @@
 import SwiftUI
 
 struct AuthenticatedView: View {
-    @State private var selection = 0
     var body: some View {
-        TabView(selection: $selection) {
-            Text("First View")
+        TabView() {
+            PlannerView()
                 .tabItem {
                     Image(systemName: "1.square.fill")
                     Text("First")
                 }
-                .tag(0)
-            
-            Text("Second View")
-                .tabItem {
-                    Image(systemName: "2.square.fill")
-                    Text("Second")
-                }
-                .tag(1)
-            
-            Text("Third View")
-                .tabItem {
-                    Image(systemName: "3.square.fill")
-                    Text("Third")
-                }
-                .tag(2)
         }
     }
 }
