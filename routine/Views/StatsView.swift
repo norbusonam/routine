1bcc9da3394475073bd23a447cd7d4dba1cf76af
norbusonam <nsonam1822@gmail.com>
2024-01-31 20:58:25 -0800

    create stats view

---
+++ routine/Views/StatsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsView.swift
+//  routine
+//
+//  Created by Norbu Sonam on 1/31/24.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
