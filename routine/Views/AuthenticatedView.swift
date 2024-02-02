3021c56e31d4b2b1779180dff67e8d4085a4a818
norbusonam <nsonam1822@gmail.com>
2024-02-01 23:47:18 -0800

    custom navbar

--- routine/Views/AuthenticatedView.swift
+++ routine/Views/AuthenticatedView.swift
@@ -8,22 +8,44 @@
 import SwiftUI
 
 struct AuthenticatedView: View {
-    @State var page: String = "planner";
+    @State var currentPageName: String = "planner";
     
     var body: some View {
-        TabView(selection: $page) {
-            PlannerView()
-                .tabItem {
-                    Image(systemName: "calendar")
-                }
-                .tag("planner")
-            StatsView()
-                .tabItem {
-                    Image(systemName: "chart.bar")
-                }
-                .tag("stats")
+        VStack {
+            // pages
+            if currentPageName == "planner" {
+                PlannerView()
+            } else if currentPageName == "stats" {
+                StatsView()
+            }
             
+            Spacer()
+            
+            // bottom bar
+            HStack {
+                TabItem(pageName: "planner", imageName: "calendar", currentPageName: $currentPageName)
+                TabItem(pageName: "stats", imageName: "chart.bar", currentPageName: $currentPageName)
+            }
         }
+    }
+}
+
+struct TabItem: View {
+    let pageName: String
+    let imageName: String
+    @Binding var currentPageName: String
+    
+    var body: some View {
+        Button(action: {
+            currentPageName = pageName
+        }, label: {
+            Spacer()
+            Image(systemName: imageName)
+                .imageScale(.large)
+                .foregroundColor(pageName == currentPageName ? .accentColor : .primary)
+            Spacer()
+        })
+        .padding()
     }
 }
 
