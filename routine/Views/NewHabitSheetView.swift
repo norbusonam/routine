42b81683b2a179162fd42d19f89e226077a677ba
norbusonam <nsonam1822@gmail.com>
2024-02-02 20:11:36 -0800

    dismiss button, fix preview

--- routine/Views/NewHabitSheetView.swift
+++ routine/Views/NewHabitSheetView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct NewHabitSheetView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+             dismiss()
+        } label: {
+            Text("Dismiss")
+        }
     }
 }
 
 #Preview {
-    NewHabitSheetView()
+    AuthenticatedView(showNewHabitSheet: true)
 }
