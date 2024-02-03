8a5100ff3a23d60b2a3e8fd99065b1abeac33245
norbusonam <nsonam1822@gmail.com>
2024-02-02 20:07:59 -0800

    new habit sheet view

---
+++ routine/Views/NewHabitSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  NewHabitSheetView.swift
+//  routine
+//
+//  Created by Norbu Sonam on 2/2/24.
+//
+
+import SwiftUI
+
+struct NewHabitSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewHabitSheetView()
+}
