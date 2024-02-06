1b57b5a5ede3010c1acb7cb0ed42d6770267ce30
norbusonam <nsonam1822@gmail.com>
2024-02-06 00:02:45 -0800

    header with profile in stats

--- routine/Views/StatsView.swift
+++ routine/Views/StatsView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct StatsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack (alignment: .top) {
+            Text("Stats")
+                .font(.largeTitle)
+                .padding(.leading)
+            Spacer()
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .padding()
+        }
     }
 }
 
