22057350d1e8430490dd6729dd784265bc90c971
norbusonam <nsonam1822@gmail.com>
2024-02-06 00:18:47 -0800

    profile sheet

---
+++ routine/Views/ProfileSheetView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileSheetView.swift
+//  routine
+//
+//  Created by Norbu Sonam on 2/6/24.
+//
+
+import SwiftUI
+
+struct ProfileSheetView: View {
+    var body: some View {
+        Button {
+        } label: {
+            Text("Sign Out")
+                .foregroundColor(.red)
+        }
+    }
+}
+
+#Preview {
+    ProfileSheetView()
+}
