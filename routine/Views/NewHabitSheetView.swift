0df14ed8e53172e40a53151b173efc9920a92e96
norbusonam <nsonam1822@gmail.com>
2024-02-15 14:05:14 -0800

    initial create habit flow

--- routine/Views/NewHabitSheetView.swift
+++ routine/Views/NewHabitSheetView.swift
@@ -7,11 +7,25 @@
 
 import SwiftUI
 
+enum NewHabitPage: Hashable {
+    case type, name, emoji, frequency
+}
+
 struct NewHabitSheetView: View {
+    @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) var dismiss
     
     @State var habit = Habit()
-    @State var page = 0
+    @State var page = NewHabitPage.type
+    
+    func onDone() {
+        modelContext.insert(habit)
+        dismiss()
+    }
+    
+    func changePage(_ nextPage: NewHabitPage) {
+        page = nextPage
+    }
     
     var body: some View {
         VStack() {
@@ -26,21 +40,123 @@ struct NewHabitSheetView: View {
                 .padding()
             }
             Spacer()
-            Text(String(page))
-            HStack {
-                if page > 0 {
-                    Button("Previous") {
-                        page = [0, page - 1].max()!
+            TabView(selection: $page) {
+                // +------+
+                // | type |
+                // +------+
+                VStack(spacing: 32) {
+                    Text("Is this a good habit you want to grow or a bad habit you want to stop?")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                    HStack {
+                        Spacer()
+                        Button("🙂") {
+                            habit.type = .good
+                            changePage(.name)
+                        }
+                        Spacer()
+                        Button("😓") {
+                            habit.type = .bad
+                            changePage(.name)
+                        }
+                        Spacer()
                     }
                 }
-                if page < 6 {
-                    Button("Next") {
-                        page = [page + 1, 6].min()!
-                    }
+                .padding()
+                .tag(NewHabitPage.type)
+                // +------+
+                // | name |
+                // +------+
+                VStack(spacing: 32) {
+                    Text("What is the habit?")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                    TextField(habit.type == .good ? "Run" : "Smoking", text: $habit.name)
+                        .font(.title3)
+                        .multilineTextAlignment(.center)
+                        .onSubmit {
+                            // TODO: validate name
+                            page = .emoji
+                        }
                 }
+                .padding()
+                .tag(NewHabitPage.name)
+                // +-------+
+                // | emoji |
+                // +-------+
+                VStack(spacing: 32) {
+                    Text("Select an emoji for your habit")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                    TextField(habit.type == .good ? "🏃‍♂️" : "🚬", text: $habit.emoji)
+                        .font(.title3)
+                        .multilineTextAlignment(.center)
+                        .onSubmit {
+                            // TODO: validate emoji
+                            changePage(.frequency)
+                        }
+                }
+                .padding()
+                .tag(NewHabitPage.emoji)
+                // +-----------+
+                // | frequency |
+                // +-----------+
+                VStack(spacing: 32) {
+                    Text("How often do you want do this habit?")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                    HStack {
+                        DayButton(dayLabel: "M", dayEnumVal: .monday, daysList: $habit.days)
+                        Spacer()
+                        DayButton(dayLabel: "T", dayEnumVal: .tuesday, daysList: $habit.days)
+                        Spacer()
+                        DayButton(dayLabel: "W", dayEnumVal: .wednesday, daysList: $habit.days)
+                        Spacer()
+                        DayButton(dayLabel: "T", dayEnumVal: .thursday, daysList: $habit.days)
+                        Spacer()
+                        DayButton(dayLabel: "F", dayEnumVal: .friday, daysList: $habit.days)
+                        Spacer()
+                        DayButton(dayLabel: "S", dayEnumVal: .saturday, daysList: $habit.days)
+                        Spacer()
+                        DayButton(dayLabel: "S", dayEnumVal: .sunday, daysList: $habit.days)
+                    }
+                    HStack {
+                        Button("Down") {
+                            habit.goal = [habit.goal - 1, 0].max()!
+                        }
+                        Spacer()
+                        Text("\(habit.goal) \(habit.goal == 1 ? "time" : "times") per day")
+                        Spacer()
+                        Button("Up") {
+                            habit.goal += 1
+                        }
+                    }
+                    Button("Done", action: onDone)
+                }
+                .padding()
+                .tag(NewHabitPage.frequency)
             }
-            Spacer()
         }
+    }
+}
+
+struct DayButton: View {
+    let dayLabel: String
+    let dayEnumVal: DayOfTheWeek
+    
+    @Binding var daysList: [DayOfTheWeek]
+    
+    var body: some View {
+        Button(dayLabel) {
+            if (daysList.contains(dayEnumVal)) {
+                daysList = daysList.filter { day in
+                    return day != dayEnumVal
+                }
+            } else {
+                daysList.append(dayEnumVal)
+            }
+        }
+        .foregroundColor(daysList.contains(dayEnumVal) ? .accent : .primary)
     }
 }
 
