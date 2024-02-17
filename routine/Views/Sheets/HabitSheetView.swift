649f8090f08d5941c89a30f36de30b91077ec231
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-02-16 19:12:48 -0800

    close and trash buttons as header

--- routine/Views/Sheets/HabitSheetView.swift
+++ routine/Views/Sheets/HabitSheetView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct HabitSheetView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
+    
     @Binding var habit: Habit
+    
+    func deleteTask() {
+        modelContext.delete(habit)
+        dismiss()
+    }
     
     var body: some View {
         VStack {
-            Text(habit.emoji)
-            Text(habit.name)
-            ForEach(habit.days, id: \.rawValue) { day in
-                Text(day.rawValue)
+            HStack {
+                Button(action: deleteTask) {
+                    Image(systemName: "trash.circle")
+                        .imageScale(.large)
+                }
+                .foregroundColor(.red)
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .imageScale(.large)
+                }
             }
-            Text("Goal: \(habit.goal)/day")
+            Spacer()
+            VStack {
+                Text(habit.emoji)
+                Text(habit.name)
+                ForEach(habit.days, id: \.rawValue) { day in
+                    Text(day.rawValue)
+                }
+                Text("Goal: \(habit.goal)/day")
+            }
+            Spacer()
         }
+        .padding()
     }
 }
 
