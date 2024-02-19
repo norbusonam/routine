fc65365b03edd2142a55eaf3e418566515aac1d5
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-02-19 12:36:51 -0800

    use builtin emoji check

--- routine/Models/Habit.swift
+++ routine/Models/Habit.swift
@@ -8,17 +8,6 @@
 import Foundation
 import SwiftData
 
-extension Character {
-    var isSimpleEmoji: Bool {
-        guard let firstScalar = unicodeScalars.first else { return false }
-        return firstScalar.properties.isEmoji && firstScalar.value > 0x238C
-    }
-
-    var isCombinedIntoEmoji: Bool { unicodeScalars.count > 1 && unicodeScalars.first?.properties.isEmoji ?? false }
-
-    var isEmoji: Bool { isSimpleEmoji || isCombinedIntoEmoji }
-}
-
 enum HabitType: String, Codable {
     case good, bad
 }
@@ -113,6 +102,6 @@ class Habit {
     }
     
     func isValid() -> Bool {
-        return name.count >= 3 && emoji.count == 1 && emoji.first!.isEmoji && days.count > 0 && goal >= 0
+        return name.count >= 3 && emoji.unicodeScalars.count == 1 && emoji.unicodeScalars.first!.properties.isEmoji && days.count > 0 && goal >= 0
     }
 }
