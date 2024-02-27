7013969c485f2fcf93e73b78df83036e2ed5992a
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-02-26 21:59:13 -0800

    enum date weekday function

--- routine/Models/Habit.swift
+++ routine/Models/Habit.swift
@@ -14,7 +14,25 @@ enum HabitType: String, Codable {
 
 enum DayOfTheWeek: String, Codable {
     case monday, tuesday, wednesday, thursday, friday, saturday, sunday
-}
+    func weekday() -> Int {
+            switch self {
+            case .monday:
+                return 1
+            case .tuesday:
+                return 2
+            case .wednesday:
+                return 3
+            case .thursday:
+                return 4
+            case .friday:
+                return 5
+            case .saturday:
+                return 6
+            case .sunday:
+                return 7
+            }
+        }
+    }
 
 enum HabitState: String {
     case success = "✅", exceeded = "👏", atLimit = "⚠️", fail = "❌", inProgress = "⏳"
